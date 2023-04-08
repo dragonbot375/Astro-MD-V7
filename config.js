e7f23e9470bc639b686b4efd54434cf464a14bc6
dragonbot375 <115867173+dragonbot375@users.noreply.github.com>
2023-04-08 07:33:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
+ALIVE_MSG: process.env.ALIVE_MSG === undefined ? '𝙷𝚒 !!! 🙂👨‍💻🖥️ /n ❖ 𝙸𝚖 ＡＬＩＶＥ 𝚗𝚘𝚠... /n ➣ 𝕄𝕪 ℕ𝕒𝕞𝕖 :- Ɗєαcσ bₒₜ➁ ᴰᶻ 𝛁 /n➣ 𝕆𝕨𝕟𝕖𝕣 :- 𝙃𝙖𝙧𝙨𝙝𝙖𝙣𝙖 𝙆 𝙋𝙧𝙚𝙢𝙖𝙡𝙖𝙡 /n ➣ ℙ𝕣𝕖𝕤𝕖𝕟𝕥𝕖𝕣 :- Mя.Ɗєαcσ /n ➣ ℂ𝕠𝕟𝕥𝕒𝕔𝕥 :- wa.me/chat:+94755982430 /n➣ 𝕋𝕚𝕜𝕥𝕠𝕜 ℙ𝕒𝕘𝕖 :- tiktok.com/@d_e_a_c_o' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://telegra.ph/file/31c9a42f8a89dc3a27784.jpg' : process.env.ALIVE_LOGO,
+FOOTER: process.env.FOOTER === undefined ? 'Ɗєαcσ bₒₜ➁ ᴰᶻ 𝛁' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝚋𝚢 Mя.Ɗєαcσᵗᵐ*' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
